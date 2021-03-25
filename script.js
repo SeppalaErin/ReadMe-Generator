4772ec4438728b3ffe5cb580f5a74b81809b96b3
Erin Seppala <aseppala98@gmail.com>
2021-03-24 22:00:53 -0400

    added requires and prompt question template

---
+++ script.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const questions = ["What is the title of your application?", "How would you describe your applicaiton?", "How can users install your application?", "How would you describe the usage of your application?", "Who were the contributors to this application?", "What type of license does this application have?", "How should users test your application?", "What is your GitHub username?", "What is your email address?"];
